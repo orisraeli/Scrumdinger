d9d3ad09e11435f14836aa57f84b51550bf86b6b
Or Israeli <isra.or@gmail.com>
2024-04-04 18:16:00 +0300

    create project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Or Israeli on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
