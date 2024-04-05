16ec6518fb608c8e7b97b654843db0a590a382b1
Or Israeli <isra.or@gmail.com>
2024-04-05 12:06:38 +0300

    crate CardView, TrailingIconLabelStyle

--- Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Views/CardView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct CardView: View {
+	let scrum: DailyScrum
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack(alignment: .leading) {
+			Text(scrum.title)
+				.font(.headline)
+				.accessibilityAddTraits(.isHeader)
+			Spacer()
+			HStack {
+				Label("\(scrum.attendees.count)", systemImage: "person.3")
+					.accessibilityLabel("\(scrum.attendees.count) attendees")
+				Spacer()
+				Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+					.labelStyle(.trailingIcon)
+					.accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+			}
+			.font(.caption)
+		}
+		.padding()
+		.foregroundStyle(scrum.theme.accentColor)
     }
 }
 
-#Preview {
-    CardView()
+struct CardView_Previews: PreviewProvider {
+	static var scrum = DailyScrum.sampleData[0]
+	static var previews: some View {
+		CardView(scrum: scrum)
+			.background(scrum.theme.mainColor)
+			.previewLayout(.fixed(width: 400, height: 60))
+	}
 }
